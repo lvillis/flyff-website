712c04d4e4a44f13682078a49051804a3d90f853
Travis Whitney <travis@edgeonline.com.au>
2020-02-11 16:22:52 +1000

    First Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,6 +30,8 @@
 </template>
 
 <script>
+  import '../assets/main.scss'
+
 export default {
   name: 'HelloWorld',
   props: {
@@ -38,20 +40,3 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
